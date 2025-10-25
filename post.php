8c41047e1e221f568e4e7c7d76548eb93820428b
LokiiiGo <lucascalle880@gmail.com>
2025-10-25 16:23:47 -0300

    Blog de Programação

--- post.php
+++ post.php
@@ -27,6 +27,7 @@ if(isset($_GET['id'])) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
   <title>Post - <?= $currentPost['title'] ?></title>
 </head>
+
 <body>
   <header>
         <a href="<?=$BASE_URL?>" id="logo">
