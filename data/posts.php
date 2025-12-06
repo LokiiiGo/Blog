ecaa21870f1cf61a5832368a6ffd5b06a024b160
LokiiiGo <lucascalle880@gmail.com>
2025-12-06 19:13:24 -0300

    Adicionandos descrições

--- data/posts.php
+++ data/posts.php
@@ -1,39 +1,95 @@
 <?php
-    $posts = [
-        [
-        'id' => 1,
-        'title' => 'Programação PHP',
-        'description' => 'PHP (abreviação para PHP: Hypertext Preprocessor) é uma linguagem de script do lado do servidor, o que significa que o código PHP é executado no servidor web antes que a página seja enviada para o navegador do usuário.
-        Isso permite que o PHP gere conteúdo dinâmico, interaja com bancos de dados e processe informações antes de exibir a página no navegador. ',
-        'tags' => ['Programação', 'PHP', 'Lógica de Programação'],
-        'img' => 'php-placa.png',
+$posts = [
+    [
+        'id'          => 1,
+        'title'       => 'Programação PHP',
+        'description' => 'PHP é uma linguagem de script do lado do servidor, o que significa que o código PHP é executado no servidor web antes que a página seja enviada para o navegador do usuário.
+        Isso permite que o PHP gere conteúdo dinâmico, interaja com bancos de dados e processe informações antes de exibir a página no navegador. 
+        <br><br>
+        PRINCIPAIS CARACTERISTICAS SOBRE PHP
+        <br><br>
+        - Uma linguagem de programação que foi desenvolvida para deixar o HTML dinâmico.
+        <br><br>
+        - Liguagem de script e open-source.
+        <br><br>
+        - Os programas são executados em server side (lado do servidor; Back-end).
+        <br><br>
+        - PHP significa Personal Home Page e HyperText Preprocessor.
+        <br><br>
+        - O criador da lingaugem foi Rasmus Lendorf.
+        <br><br>
+        - O ano de lançamento foi em 1994.
+        <br><br>
+        - Aproximadamente 80% dos websites utilizam PHP.',
+        'tags'        => ['Programação', 'PHP', 'Lógica de Programação'],
+        'img'         => 'PHP.png',
     ],
 
     [
-        'id' => 2,
-        'title' => 'Programação orientada a objetos com Python',
+        'id'          => 2,
+        'title'       => 'Programação orientada a objetos com Python',
         'description' => 'A Programação Orientada a Objetos (POO) em Python é um paradigma de programação que organiza o código em objetos, que são instâncias de classes. As classes definem as características
         (atributos) e ações (métodos) que os objetos podem ter. A POO em Python permite criar programas reutilizáveis, fáceis de manter e que modelam melhor a realidade, tornando-a uma opção popular para projetos complexos. ',
-        'tags' => ['OOP', 'Python', 'Programação Avançada'],
-        'img' => 'python.png',
+        'tags'        => ['OOP', 'Python', 'Programação Avançada'],
+        'img'         => 'python.png',
 
     ],
 
     [
-        'id' => 3,
-        'title' => 'A importância da Lógica de programação',
-        'description' => 'A lógica de programação é o conjunto de regras e técnicas que os programadores utilizam para projetar e desenvolver programas de computador. 
+        'id'          => 3,
+        'title'       => 'A importância da Lógica de programação',
+        'description' => 'A lógica de programação é o conjunto de regras e técnicas que os programadores utilizam para projetar e desenvolver programas de computador.
         É a habilidade de pensar de forma lógica e estruturada, decompondo um problema complexo em etapas mais simples.',
-        'tags' => ['Lógica', 'Programação', 'Algoritmos'],
-        'img' => 'LogicaProgramacao.jpg',
+        'tags'        => ['Lógica', 'Programação', 'Algoritmos'],
+        'img'         => 'LogicaProgramacao.jpg',
     ],
 
     [
-        'id' => 4,
-        'title' => 'Programando em Bootstrap',
+        'id'          => 4,
+        'title'       => 'Programando em Bootstrap',
         'description' => 'Bootstrap é um framework front-end que fornece estruturas de CSS para a criação de sites e aplicações responsivas de forma rápida e simples. Além disso, pode lidar com sites de desktop e páginas de dispositivos móveis da mesma forma.',
-        'tags' => ['Bootstrap', 'Programação Front-End', 'CSS Bootstrap'],
-        'img' => 'bootstrap5.png',
+        'tags'        => ['Bootstrap', 'Programação Front-End', 'CSS Bootstrap'],
+        'img'         => 'bootstrap5.png',
+    ],
+    [
+        'id'          => 5,
+        'title'       => 'Aprendendo Portugol',
+        'description' => 'É uma linguagem de programação didática que usa a lingua portuguesa como base para facilitar o aprendizadoda lógica de programação.
+        <br><br>
+        - É uma pseudolinguagem que permite a implementação de algoritmos em português de forma simples e intuitiva.
+        <br><br>
+        - A sintaxe é similar ao português, o que facilita o entendimento dos conceitos complexos.
+        <br><br>
+        - É uma ferramenta educacional, idealizada para quem está começando a aprender programação.
+        <br><br>
+        O nome Portugol é uma sigla que vem de "pseudo-código em português" e é uma junção das palavras Português, Algol e Pascal.
+        <br><br>
+        Foi criado pelos professores Antonio Carlos Nicolodi e Antonio Mannso, com o objetivo de ensinar programação sem que uma estrutura de uma linguagem passa-se sobre os iniciantes',
+        'tags'        => ['Lógica de Programação', 'Portugol', 'Lingua Portuguesa'],
+        'img'         => 'portugolIDE1.png',
+    ],
+    [
+        'id' => 6,
+        'title' => 'MySQL - Banco de Dados Relacional',
+        'description' => 'É um sistema de gerenciador de bancode dados relacional de código aberto usado na maioria das aplicações gratuitas
+        para gerir suas bases de dados. O serviço utiliza a linguagem SQL (Structure Query Language) traduzido para Lingaugem de Consulta Estruturada, que
+        é uma linguagem mais popular pra inserir, acessar e gerenciar o conteúdo armazenado num banco de dados.
+        <br><br>
+        O MySQL é usado em uma ampla variedade de aplicações como: 
+        <br><br>
+        - Sites de comércios eletrônicos.
+        <br><br>
+        - Mídias sociais.
+        <br><br>
+        - Apps como: Drupal, Joomia, Magento e WordPress.
+        <br><br>
+        - Pilha de aplicativos da web Linux-Apache-MySQL-PHP/Perl/Python.
+        <br><br>
+        - Facebook, X (atual Twitter), Netflix, Uber, Shopify e Booking.com
+        <br><br>
+        Funciona em um modelo cliente-servidor, onde os dados ficam armazenados no servidor e os clientes fazem solicitações para acessá-los.
+        Os clientes podem fazer solicitações atravéz de uma Interface Gráfica do Usuário (IGU) em suas telas.',
+        'tags' => ['Banco de Dados', 'Mysql', 'SQL'],
+        'img' => 'mysql.png',
     ],
 ];
-?>
